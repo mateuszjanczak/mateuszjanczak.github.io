f3a2699941e384ba3b9d384d15db56100ba73cea
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-03-16 20:13:07 +0100

    New links to wheel

--- assets/js/heroku.js
+++ assets/js/heroku.js
@@ -5,13 +5,12 @@ const endpoints = [
     "https://mj-community.herokuapp.com/",
     "https://smpwl-client.herokuapp.com/",
     "https://smpwl-server.herokuapp.com/",
-    "https://grow-habits.herokuapp.com/",
-    "https://grow-habits-service.herokuapp.com/",
+    "https://mj-wheel-of-fortune.herokuapp.com/",
+    "https://mj-wheel-of-fortune-service.herokuapp.com/",
     "https://mj-websocket.herokuapp.com/",
     "https://mj-notepad.herokuapp.com/",
     "https://mj-notepad-service.herokuapp.com",
     "https://ips-c.herokuapp.com/",
-    "https://ips-s.herokuapp.com/",
     "https://ips-s.herokuapp.com/",
     "https://songlyricsapi.herokuapp.com/"
 ]
